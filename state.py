0042f232801299182aa4897a7b1939d7f906e1a7
joshua-ns-jordan <joshuansjordan@gmail.com>
2018-04-07 01:42:08 +0000

    states and clicks working with real thing

--- state.py
+++ state.py
@@ -1,14 +1,15 @@
-#import actuator
+import actuator
 import threading
 import time
 import random
+import cv2
 
 
 class State:
     def __init__(self, prior):
         self.prior_s = prior
 
-    def run(self):
+    def run(self, actuator):
         assert 0, "run not implemented"
 
     def next(self):
@@ -19,18 +20,22 @@ class State:
             return self.prior_s
         else:
             return self
+        
+    def stop(self):
+        return Stop(self)
 
 
 class Out(State):
-    def run(self):
+    def run(self, actuator):
         print('out')
+        actuator.move_time(2, ['gaa-out'])
 
     def next(self):
         return Locate(self)
 
 
 class Locate(State):
-    def run(self):
+    def run(self, actuator):
         print('locate')
 
     def next(self):
@@ -38,7 +43,7 @@ class Locate(State):
 
 
 class Up(State):
-    def run(self):
+    def run(self, actuator):
         print('up')
 
     def next(self):
@@ -46,7 +51,7 @@ class Up(State):
 
 
 class In(State):
-    def run(self):
+    def run(self, actuator):
         print('in')
 
     def next(self):
@@ -54,15 +59,16 @@ class In(State):
 
 
 class Stop(State):
-    def run(self):
+    def run(self, actuator):
         print('stop')
-
+        actuator.stop()
+        
     def next(self):
         return Reset(self)
 
 
 class Reset(State):
-    def run(self):
+    def run(self, actuator):
         print('reset')
 
     def next(self):
@@ -71,42 +77,38 @@ class Reset(State):
 
 class StateMachine:
     def __init__(self):
-        #self.actuator = actuator.Actuator()
-        t = threading.Thread(target=self.mouse_thread)
-        #t.start()
-
+        self.actuator = actuator.Actuator()
         self.state = Reset(None)
+        self.state_change = False
+        
+        cv2.namedWindow("click")
+        cv2.waitKey(1)
+        cv2.destroyWindow("click")
 
+        cv2.namedWindow("click")#, cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty("click", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.setMouseCallback("click", self.mouse_event)
+
+    def start(self):
         while True:
-            self.state.run()
-            time.sleep(1)
-            if random.getrandbits(1):
-                self.state = self.state.prior()
-            else:
-                self.state = self.state.next()
+            cv2.waitKey(1)
+            if self.state_change:
+                print()
+                t = threading.Thread(target=self.state.run, args=(self.actuator,))
+                t.start()
+                self.state_change = False
 
-    def mouse_thread(self):
-        while True:
-            left, middle, right = self.get_mouse_event()
-            if left != 0 or right != 0 or middle != 0:
-                print(left, middle, right)
-
-    @staticmethod
-    def get_mouse_event():
-        with open("/dev/input/mice", "rb") as f:
-            buf = f.read(3)
-            button = ord(buf[0])
-            return button & 0x1, (button & 0x4) > 0, (button & 0x2) > 0
+    def mouse_event(self, event, x, y, flags, param):
+        if event == 6:
+            self.state_change = True
+            self.state = self.state.stop()
+        elif event == 4:
+            self.state_change = True            
+            self.state = self.state.next()
+        elif event == 5:
+            self.state_change = True            
+            self.state = self.state.prior()
 
 sm = StateMachine()
+sm.start()
 
-# def mouse_event(event, x, y, flags, param):
-# cv2.namedWindow("click")
-# cv2.waitKey(1)
-# cv2.destroyWindow("click")
-#
-# cv2.namedWindow("click")#, cv2.WND_PROP_FULLSCREEN)
-# cv2.setWindowProperty("click", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-# cv2.setMouseCallback("click", mouse_event)
-#
-# cv2.waitKey(0)
